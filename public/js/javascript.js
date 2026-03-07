ca77b1e7596ff30142cb619bfe8600864c01d496
MiaroNandrianina <myaronandrianina@gmail.com>
2026-03-07 10:45:45 +0300

    feat(main):added code js and css code to create a password in contact

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,6 +1,10 @@
 let darkMode = document.querySelector(".darkMode")
 let hero =document.querySelector(".hero")
 const bgIcone =document.querySelector(".bgIcone")
+const affichage =document.getElementById("affichage")
+
+let password =document.getElementById("password")
+let togglePassword=document.getElementById("togglePassword")
 darkMode.addEventListener("click",() => {
 
     if (hero.style.background==="white") {
@@ -18,3 +22,34 @@ darkMode.addEventListener("click",() => {
         darkMode.innerHTML="on"
     }
 })
+
+togglePassword.addEventListener("click",() =>{
+    if (password.type ==="password") {
+        password.type="text"
+        togglePassword.classList.replace("fa-eye" ,"fa-eye-slash")
+    }else{
+        password.type="password"
+        togglePassword.classList.replace("fa-eye-slash","fa-eye")
+    }
+})
+password.addEventListener("input",() =>{
+    let refyPassword =password.value.length
+
+    if (refyPassword  > 0 && refyPassword < 4) {
+        affichage.innerHTML = "faible"
+        affichage.style.color="red"
+        password.style.borderColor="red"
+    }else if (refyPassword >=4 && refyPassword <6 ) {
+        password.style.borderColor="yellow"
+        affichage.style.color="yellow"
+        affichage.innerHTML ="moyenne"
+    }else if (refyPassword >=6 ) {
+        password.style.borderColor="green"
+        affichage.innerHTML ="forte"
+        affichage.style.color="green"
+    }else{
+         password.style.borderColor=""
+        affichage.innerHTML=""
+    }
+})
+
